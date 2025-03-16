bd3e7f482c941cf9e76c4a163af295fd6a587797
b4rp <ryverjr@gmail.com>
2025-03-16 20:50:23 -0300

    finally

--- config/polybar/shapes/scripts/vpn_status.sh
+++ config/polybar/shapes/scripts/vpn_status.sh
@@ -12,7 +12,7 @@ grayColour="\e[0;37m\033[1m"
 IFACE=$(/usr/sbin/ifconfig | grep tun0 | awk '{print $1}' | tr -d ':')
 
 if [ "$IFACE" = "tun0" ]; then
-	echo "%{F#ffffff}  %{F#ffffff}$(/usr/sbin/ifconfig tun0 | grep "inet " | awk '{print $2}')%{u-}"
+	echo "%{F#32cd32} %{F#ffffff}$(/usr/sbin/ifconfig tun0 | grep "inet " | awk '{print $2}')%{u-}"
 else
-	echo "%{F#ffffff} %{u-} Disconnected"
+	echo "%{F#ff0000}%{u-} Disconnected"
 fi
