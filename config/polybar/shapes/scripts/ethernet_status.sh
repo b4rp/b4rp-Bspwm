ad20e7b616d0adee594a152f12cb91d676be53b5
b4rp <ryverjr@gmail.com>
2025-03-16 21:17:17 -0300

    Update colors.conf

--- config/polybar/shapes/scripts/ethernet_status.sh
+++ config/polybar/shapes/scripts/ethernet_status.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo "%{F#ffffff}  %{F#ffffff}$(/usr/sbin/ifconfig eth0 | grep "inet " | awk '{print $2}')%{u-}"
+echo "%{F#7dcfff}  %{F#ffffff}$(/usr/sbin/ifconfig eth0 | grep "inet " | awk '{print $2}')%{u-}"
--- scripts/htb_target.sh
+++ scripts/htb_target.sh
@@ -4,9 +4,9 @@ ip_target=$(cat ~/.config/polybar/shapes/scripts/target | awk '{print $1}')
 name_target=$(cat ~/.config/polybar/shapes/scripts/target | awk '{print $2}')
 
 	if [ $ip_target ] && [ $name_target ]; then
-		echo "%{F#ffffff}什%{F#ffffff} $ip_target - $name_target "
+		echo "%{F#e51d0b}ﲅ%{F#ffffff} $ip_target - $name_target "
 	elif [ $(cat ~/.config/polybar/shapes/scripts/target | wc -w) -eq 1 ]; then
-		echo "%{F#ffffff}什%{F#ffffff} $ip_target "
+		echo "%{F#e51d0b}ﲅ%{F#ffffff} $ip_target "
 	else
-		echo "%{F#ffffff}什%{u-}%{F#ffffff} No target "
+		echo "%{F#e51d0b}ﲅ%{u-}%{F#ffffff} No target "
 	fi
